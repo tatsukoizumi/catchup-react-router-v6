467f27b06ff73def6adab2190628c906f28b6bdf
tatsukoizumi <koizumitattsuu@gmail.com>
2022-09-24 14:07:35 +0900

    add root element

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,10 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom"
+import { Root } from "../routes/Root"
 
-const router = createBrowserRouter(createRoutesFromElements(<Route></Route>))
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Root />}></Route>)
+)
 
 export const AppRouter = () => <RouterProvider router={router} />
